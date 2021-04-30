efe9c511b3145c40223dd31321245eb53c594d4e
Dhanushu1999 <Dhanushu1999@gmail.com>
2021-04-30 10:19:10 +0530

    added documentation

--- src/activity4.c
+++ src/activity4.c
@@ -7,21 +7,21 @@
 
 void Init_USART()
 {
-    UBRR0H = (uint8_t)(UBRR_VALUE>>8);
+    UBRR0H = (uint8_t)(UBRR_VALUE>>8); /**< Set BAUD Rate */
     UBRR0L = (uint8_t)UBRR_VALUE;
-    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);/**< Enable Transmitter and Receiver */
     UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
 
 int USARTRead()
 {
-    while(!(UCSR0A &(1<<RXC0)))
+    while(!(UCSR0A &(1<<RXC0))) /**< Check if the data is received into the buffer or not*/
     {
         //Do nothing
     }
     return UDR0;
 }
-void UARTWrite(char data){
+void UARTWrite(char data){ /**< Check if the data is not written into the buffer*/
     while(!( UCSR0A & (1<<UDRE0))){
 
     }
